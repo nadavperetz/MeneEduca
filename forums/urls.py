28f84a65928a7b68a8dd2ac13d882385d771728f
nadavperetz <nadavperetz@gmail.com>
2014-07-07 17:52:33 -0300

    modificacoes iniciais

--- forums/urls.py
+++ forums/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 
 
 urlpatterns = patterns("",
+    url(r"^$", "forums.views.index", name="index"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
     url(r"^profiles/", include("forums.profiles.urls")),
