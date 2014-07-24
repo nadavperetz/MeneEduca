ae85824bf26d8996d7342aa70ad8641c75ca1fc5
nadavperetz <nadavperetz@gmail.com>
2014-07-24 11:38:46 -0300

    Excluir algums campos de edicao de perfil

--- forums/profiles/forms.py
+++ forums/profiles/forms.py
@@ -7,4 +7,9 @@ class ProfileForm(forms.ModelForm):
 
     class Meta:
         model = Profile
+        exclude = ["user",
+                   "complete_profile",
+                   "groups",
+                   "created_at",
+                   "modified_at"]
 
