acabae70bae5c81126c8c4d1c2a271964bf13afd
nadavperetz <nadavperetz@gmail.com>
2014-07-31 17:58:46 -0300

    acerto namespace urls

--- MeneEduca/urls.py
+++ MeneEduca/urls.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
                        url(r"^$", "MeneEduca.views.index", name="index"),
                        url(r"^admin/", include(admin.site.urls)),
-                       url(r"^account/", include("account.urls", namespace='account')),
+                       url(r"^account/", include("account.urls")),
                        url(r"^profiles/", include("profiles.urls", namespace='profiles')),
                        url(r"^survey/", include("survey.urls", namespace='survey')),
                        url(r"^forums/", include("agora.urls", namespace='forums'))
