99452785c6bc4a0bceef690e18f5880909cc08c4
nadavperetz <nadavperetz@gmail.com>
2014-11-22 11:22:11 -0200

    correcao codigo para seguir boas praticas

--- groups/utils.py
+++ groups/utils.py
@@ -1,16 +1,20 @@
-from groups.models import Group
+
 
 def bigger_groups_number(npeople, ngroups):
     return npeople % ngroups
 
+
 def smaller_groups_number(npeople, ngroups):
     return ngroups - npeople % ngroups
+
 
 def bigger_groups_size(npeople, ngroups):
     return npeople // ngroups + 1
 
+
 def smaller_groups_size(npeople, ngroups):
     return npeople // ngroups
+
 
 def dfs_group_formation(people, ngroups):
     groups = []
@@ -18,6 +22,7 @@ def dfs_group_formation(people, ngroups):
         groups.append([])
 
     return dfs_group_formation2(len(people), people, groups)
+
 
 def dfs_group_formation2(npeople, people, groups):
     bigger_number = bigger_groups_number(npeople, len(groups))
@@ -28,11 +33,11 @@ def dfs_group_formation2(npeople, people, groups):
     best_solution = (0.0, None)
 
     # base of the recursion
-    if people == []:
+    if not people:
         groups_copy = []
         for group in groups:
             groups_copy.append(list(group))
-        return (rate_formation(groups), list(groups_copy))
+        return rate_formation(groups), list(groups_copy)
 
     # takes the first person
     person = people.pop()
@@ -68,6 +73,7 @@ def dfs_group_formation2(npeople, people, groups):
     people.append(person)
     return best_solution
 
+
 def rate_group(group):
     max_traits = [0.0, 0.0, 0.0, 0.0, 0.0]
     min_traits = [1.0, 1.0, 1.0, 1.0, 1.0]
@@ -83,13 +89,14 @@ def rate_group(group):
 
     rate = 1.0
     for i in range(len(max_traits)):
-        rate = rate * (max_traits[i] - min_traits[i])
-    rate = rate ** (1.0 / len(max_traits))
+        rate *= (max_traits[i] - min_traits[i])
+    rate **= (1.0 / len(max_traits))
     return rate
+
 
 def rate_formation(groups):
     rate = 1.0
     for group in groups:
         rate = rate * rate_group(group)
-    rate = rate ** (1.0 / len(groups))
+    rate **= (1.0 / len(groups))
     return rate
