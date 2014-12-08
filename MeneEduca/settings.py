f6aca1e8d37ab7cdbf5da6a5542d73e307d2b056
nadavperetz <nadavperetz@gmail.com>
2014-12-08 09:34:42 -0200

    mudancas novo layout

--- MeneEduca/settings.py
+++ MeneEduca/settings.py
@@ -64,7 +64,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, "site_media", "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/site_media/static/"
+STATIC_URL = "/static/"
 
 # Additional locations of static files
 STATICFILES_DIRS = [
--- MeneEduca/views.py
+++ MeneEduca/views.py
@@ -4,4 +4,4 @@ from account.decorators import login_required
 
 @login_required
 def index(request):
-    return render(request, 'site_base.html')
+    return render(request, 'index.html')
