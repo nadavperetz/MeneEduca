63a68c70c30266453fead74d54371dd4d583eb21
Vitor Meneguzzi Groterhorst <vitor023@gmail.com>
2014-12-08 15:37:45 -0200

    Assignment create view

--- educational/urls.py
+++ educational/urls.py
@@ -2,12 +2,11 @@ from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
 
-from .views.teachers import DisciplineUpdateView, DisciplineDetailView
-
+from .views.teachers import DisciplineUpdateView, DisciplineDetailView, AssignmentCreateView
 
 urlpatterns = patterns(
     "",
     url(r"^disciplines/(?P<pk>\d+)/$", DisciplineDetailView.as_view(), name="discipline_detail"),
     url(r"^disciplines/edit/(?P<pk>\d+)/$", DisciplineUpdateView.as_view(), name="discipline_update"),
-
+    url(r"^disciplines/(?P<discipline_id>\d+)/assignments/create/$", AssignmentCreateView.as_view(), name="assignment_create"),
 )
--- educational/views/teachers.py
+++ educational/views/teachers.py
@@ -3,7 +3,7 @@ from django.forms.models import inlineformset_factory
 from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from django.shortcuts import get_object_or_404, redirect, render
 
-from educational.models import Discipline
+from educational.models import Discipline, Assignment
 
 class DisciplineDetailView(DetailView):
     model = Discipline
@@ -18,6 +18,25 @@ class DisciplineUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('educational:discipline_detail', kwargs={'pk': self.object.pk})
 
+class AssignmentCreateView(CreateView):
+    model = Assignment
+    template_name = 'educational/teacher/assignment_create_view.html'
+    fields = ['title']
+
+    def get_context_data(self, **kwargs):
+        context = super(AssignmentCreateView, self).get_context_data(**kwargs)
+        context['discipline'] = Discipline.objects.get(pk=self.kwargs['discipline_id'])
+        return context
+
+    def form_valid(self, form):
+        form.instance.discipline = Discipline.objects.get(pk=self.kwargs['discipline_id'])
+        return super(AssignmentCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('educational:discipline_detail', kwargs={'pk': self.kwargs['discipline_id']})
 
 
+def assignment_create(request, discipline_id):
+    discipline = get_object_or_404(Discipline, pk=discipline_id)
+    return render(request, 'educational/teacher/assignment_create.html', {'discipline': discipline})
 
