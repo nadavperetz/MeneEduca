c87abebb17239c5be8cc06c2c468c0b3b3c85cab
nadavperetz <nadavperetz@gmail.com>
2014-12-11 19:58:36 -0200

    encoding

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 
 indicator_choices = (('Extroversion ', _('Extroversion (1)')),
                      ('Agreeableness', _('Agreeableness (2)')),
@@ -19,6 +20,7 @@ indicator_weight = (('+', '+'),
                     ('-', '-'))
 
 
+@python_2_unicode_compatible
 class QuestionnaireModel(models.Model):
     name = models.CharField(max_length=50, verbose_name=_("Name"))
 
@@ -26,6 +28,7 @@ class QuestionnaireModel(models.Model):
         return self.name
 
 
+@python_2_unicode_compatible
 class QuestionModel(models.Model):
     question = models.CharField(max_length=50, verbose_name=_("Question"))
     type_of_answer = models.CharField(max_length=30, verbose_name=_("Type of Factor"),
@@ -37,6 +40,7 @@ class QuestionModel(models.Model):
         return self.question
 
 
+@python_2_unicode_compatible
 class QuestionnaireAnswered(models.Model):
     questionnaire = models.ForeignKey(QuestionnaireModel)
     student = models.ForeignKey('profiles.Student')
@@ -74,6 +78,7 @@ class QuestionnaireAnswered(models.Model):
         return text
 
 
+@python_2_unicode_compatible
 class QuestionAnswered(models.Model):
     question = models.ForeignKey(QuestionModel)
     answer = models.IntegerField(choices=answers, blank=True, null=True)
