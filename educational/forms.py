fd6d2e851a81149452b1e18ccc8defd691fb34ab
Vitor Meneguzzi Groterhorst <vitor023@gmail.com>
2014-12-12 01:05:32 -0200

    Adicionado help text para a criação de grupos

--- educational/forms.py
+++ educational/forms.py
@@ -6,3 +6,4 @@ class NameForm(forms.Form):
         super(NameForm, self).__init__(*args, **kwargs)
         self.fields['name'] = forms.CharField(label=_(u'Name'), max_length=100)
         self.fields['students'] = forms.MultipleChoiceField(label=_(u'Students'), choices=[(p.pk, p) for p in profiles])
+        self.fields['students'].help_text = _('Hold down "Control", or "Command" on a Mac, to select more than one.')
