be895434b3b8f97d82d534bf105268c114421809
nadavperetz <nadavperetz@gmail.com>
2014-12-15 23:16:19 -0200

    acertos finais

--- educational/views/guardians.py
+++ educational/views/guardians.py
@@ -1,10 +1,6 @@
-from django.core.urlresolvers import reverse
-from django.forms.models import inlineformset_factory
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
-from django.shortcuts import get_object_or_404, redirect, render
-from django.http import HttpResponseRedirect
-
+from django.views.generic import DetailView
 from educational.models import Assignment
+
 
 class AssignmentDetailView(DetailView):
     model = Assignment
